ddb2acf3c7c90141040afa61908df4640d274ae5
KotSergiy <ks77vl@gmail.com>
2021-02-21 21:15:09 +0200

    Add print to console

--- test1.rb
+++ test1.rb
@@ -12,17 +12,17 @@ led_green = pi.gpio(27)
 led_green.mode = PI_OUTPUT
 
 10.times do |i|
+  print "*r"
   led_red.write 1
   led_green.write 0
   sleep 0.5
+  print "*g"
   led_red.write 0
-  led_green.write 1
-  sleep 0.5
-  led_red.write 1
   led_green.write 1
   sleep 0.5
 end
 
+print "*end"
 led_red.write 0
 led_green.write 0
 pi.stop
