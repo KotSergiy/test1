a6df761cf9beb5cf8f7e0c42ec092cc1422be7e4
KotSergiy <ks77vl@gmail.com>
2021-02-21 21:07:42 +0200

    When end, leds off

--- test1.rb
+++ test1.rb
@@ -10,6 +10,7 @@ led_red = pi.gpio(4)
 led_red.mode = PI_OUTPUT
 led_green = pi.gpio(27)
 led_green.mode = PI_OUTPUT
+
 10.times do |i|
   led_red.write 1
   led_green.write 0
@@ -21,4 +22,7 @@ led_green.mode = PI_OUTPUT
   led_green.write 1
   sleep 0.5
 end
+
+led_red.write 0
+led_green.write 0
 pi.stop
