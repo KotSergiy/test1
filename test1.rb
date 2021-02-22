a61188f38f902195f8f98771e10108837b112821
KotSergiy <ks77vl@gmail.com>
2021-02-22 21:25:16 +0200

    use green led

--- test1.rb
+++ test1.rb
@@ -8,31 +8,31 @@ end
 
 led_red = pi.gpio(4)
 led_red.mode = PI_OUTPUT
+led_red.write 0
+
 led_green = pi.gpio(27)
 led_green.mode = PI_OUTPUT
+led_green.write 0
 
 button=pi.gpio(22)
 button.mode=PI_INPUT
 button.pud=PI_PUD_UP
 button.glitch_filter(5000)
 
-counter=0
 cb=button.callback(EITHER_EDGE){|tick,level|
   led_red.write level
-  counter+=1
+  led_green.write !(level)
   print '*'
 }
 
 led_red.write 1
-while(counter<10)do
-  sleep(1)
-end
-puts
-cb.cancel
 while true do
   if (gets.strip.downcase=='stop')
     break
   end
 end
+puts
+cb.cancel
 led_red.write 0
+led_green.write 0
 pi.stop
