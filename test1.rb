88e48e1e9e6257e662de644af2ec7682830c1cbb
KotSergiy <ks77vl@gmail.com>
2021-02-22 20:38:12 +0200

    Add button

--- test1.rb
+++ test1.rb
@@ -11,18 +11,23 @@ led_red.mode = PI_OUTPUT
 led_green = pi.gpio(27)
 led_green.mode = PI_OUTPUT
 
-10.times do |i|
-  print "*"
-  led_red.write 1
-  led_green.write 0
-  sleep 0.5
-  print "*"
-  led_red.write 0
-  led_green.write 1
-  sleep 0.5
-end
+button=pi.gpio(22)
+button.mode=PI_INPUT
+button.pud=PI_PUD_UP
+button.glitch_filter(30)
 
+counter=0
+cb=button.callback(EITHER_EDGE){|tick,level|
+  led_red.write level
+  counter+=1
+  print '*'
+}
+
+led_red.write 1
+while(counter<10)do
+  sleep(1)
+end
 puts
+cb.cancel
 led_red.write 0
-led_green.write 0
 pi.stop
